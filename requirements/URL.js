e7bba4acf6b48a30c6edd1b2a6e330e6950bf17c
Damien Maillard <dmaillard06@gmail.com>
2015-06-23 12:06:07 +0200

    add url.searchParams for convenience

--- requirements/URL.js
+++ requirements/URL.js
@@ -1,6 +1,110 @@
 (function(global){
-	// https://gist.github.com/Yaffle/1088850
+	// https://github.com/WebReflection/url-search-params/tree/master/src
+	var replace = {
+		'!': '%21',
+		"'": '%27',
+		'(': '%28',
+		')': '%29',
+		'~': '%7E',
+		'%20': '+',
+		'%00': '\x00'
+	};
+
+	function replacer(match){
+		return replace[match];
+	}
+
+	function encode(str){
+		return encodeURIComponent(str).replace(/[!'\(\)~]|%20|%00/g, replacer);
+	}
+
+	function decode(str) {
+		return decodeURIComponent(str.replace(/\+/g, ' '));
+	}
+
+	var URLSearchParams = Function.create({
+		constructor: function(queryString){
+			this.fromString(queryString);
+		},
+
+		fromString: function(queryString){
+			this.params = {};
+
+			if( queryString ){
+				var index, value, pairs = queryString.split('&'), i = 0, length = pairs.length;
+
+				for(;i<length;i++){
+					value = pairs[i];
+					index = value.indexOf('=');
+					if( index > -1 ){
+						this.append(
+							decode(value.slice(0, index)),
+							decode(value.slice(index + 1))
+						);
+					}
+				}
+			}
+		},
+
+		append: function(name, value){
+			var params = this.params;
+
+			value = String(value);
+			if( name in params ){
+				params[name].push(value);
+			}
+			else{
+				params[name] = [value];
+			}
+		},
+
+		delete: function(name){
+			delete this.params[name];
+		},
+
+		get: function(name){
+			var params = this.params;
+  			return name in params ? params[name][0] : null;
+		},
+
+		getAll: function(name){
+			var params = this.params;
+  			return name in params ? params[name].slice(0) : [];
+		},
+
+		has: function(name){
+			return name in this.params;
+		},
+
+		set: function(name, value){
+			value = String(value);
+			this.params[name] = [value];
+		},
+
+		toJSON: function(){
+			return {};
+		},
+
+		toString: function(){
+			var params = this.params, query = [], key, name, i, value, j;
+
+			for(key in params) {
+				name = encode(key);
+				i = 0;
+				value = params[key];
+				j = value.length;
+
+				for(;i<j;i++){
+					query.push(name + '=' + encode(value[i]));
+				}
+			}
+
+			return query.join('&');
+		}
+	});
+
 	// https://github.com/Polymer/URL/blob/master/url.js
+	// https://gist.github.com/Yaffle/1088850
 	function parseURL(url){
 		if( typeof url === 'object' ) return url;
 		if( url == null ) throw new TypeError(url + 'is not a valid url');
@@ -93,6 +197,8 @@
 				}
 			}
 
+			this.searchParams = new URLSearchParams();
+
 			this.protocol = url.protocol;
 			this.username = url.username;
 			this.password = url.password;
@@ -106,6 +212,14 @@
 			this.origin = this.protocol;
 			if( this.protocol || this.host ) this.origin+= '//';
 			this.origin+= this.host;
+		},
+
+		get search(){
+			return this.searchParams.toString();
+		},
+
+		set search(value){
+			this.searchParams.fromString(value);
 		},
 
 		toString: function(){
